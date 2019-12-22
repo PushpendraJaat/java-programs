b61b85c8fd14d77bf6829afaa065aeaca13d9d88
Pushpendra Jaat <57589014+PushpendraJaat@users.noreply.github.com>
2019-12-22 15:33:39 +0530

    Taking input from the user

--- Inputexample.java
+++
@@ -1,1 +0,0 @@
-import java.util.*;class Inputexample{ public static void main(String []args)	 {	 System.out.println("Type your name and age");	 Scanner sc=new Scanner(System.in);     String name=sc.next();     int age=sc.nextInt();     System.out.println("Name:"+name+"Age:"+age);	 }}
